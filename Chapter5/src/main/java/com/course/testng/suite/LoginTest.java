144db8d0229577fce7634e08d99aaddfbb28adca
yangfan <yangfan921106@163.com>
2021-11-26 13:35:07 +0800

    idea提交

---
+++ Chapter5/src/main/java/com/course/testng/suite/LoginTest.java
@@ -0,0 +1,11 @@
+package suite;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @Test
+    public void loginTaoBao(){
+        System.out.println("淘宝登录成功啦");
+    }
+}
---
+++ Chapter5/src/main/java/com/course/testng/suite/PayTest.java
@@ -0,0 +1,12 @@
+package suite;
+
+import org.testng.annotations.Test;
+
+
+public class PayTest {
+
+    @Test
+    public void paySucces(){
+        System.out.println("支付宝支付成功");
+    }
+}
